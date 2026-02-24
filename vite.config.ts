746159b7c667961ef242acb7a774badffc1a8843
Vladislav Sviderski <svidvlad15@gmail.com>
2026-02-24 23:16:25 +0300

    push deployments config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base:
+    process.env.GITHUB_PAGES === 'true'
+      ? `/${process.env.GITHUB_REPOSITORY?.split('/').pop() ?? 'LeadProGroup'}/`
+      : '/',
   plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
